9342aeb7ea43ef7122d6efd145f0f70327ac5aa5
e248768 <e248768@sbl.ch>
2022-11-16 14:51:31 +0100

    color change

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function handleKeyPress(ev) {
+function handleKeyPress (ev) {
     if (ev.code === "Space") {
-         const player =
-        document.querySelector("#player")
+         const player = document.querySelector("#player")
         player.setAttribute("cx", 100 * Math.random())
         player.setAttribute("cy", 100 * Math.random())
+    } else if (ev.key === "r") {
+            player.setAttribute ("fill", "red")
+    } else if (ev.key === "g") {
+            player.setAttribute ("fill", "lightgreen")
     }
 }
 
