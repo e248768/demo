653e54d29ffc508ea1492b81ad3b067a49074371
e248768 <e248768@sbl.ch>
2022-11-16 14:34:55 +0100

    random jumping

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleKeyPress(ev) {
+    const player =
+    document.querySelector("#player")
+    player.setAttribute("cx", 100 * Math.random())
+    player.setAttribute("cy", 100 * Math.random())
+}
+
+
+window.onkeydown = handleKeyPress
